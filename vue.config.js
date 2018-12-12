18d52358a2708884db573cad08fbccf803b07d9d
Zayne.Liu <llz981011@hotmail.com>
2018-12-12 16:30:01 +0800

    Config

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: '/movies/',
+  outputDir: undefined,
+  assetsDir: 'src',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
