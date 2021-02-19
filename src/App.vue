6a32034b315f073979b41ebb7336d79786de82d6
Zayne Liu <llz981011@hotmail.com>
2021-02-19 14:23:19 +0000

    wrap up

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <Marvel />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import Marvel from "./views/Marvel.vue";
+
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      (window.pageYOffset / window.innerHeight).toFixed(3).toString()
+    );
+    console.log("offset", window.pageYOffset / window.innerHeight);
+    console.log(window.innerHeight);
+  },
+  false
+);
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-})
+    HelloWorld,
+    Marvel,
+  },
+});
 </script>
 
 <style>
@@ -22,6 +38,7 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* height: 100vh; */
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/views/Marvel.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="viewport">
+    <div class="white box"></div>
+    <div class="black box"></div>
+  </div>
+
+  <div class="arrow-down" alt="" @click="scrollBtnOnClick()"></div>
+</template>
+
+<style >
+.viewport {
+  z-index: 0;
+  height: 200vh;
+  background-color: #000;
+}
+
+.box {
+  position: fixed;
+  height: 100%;
+
+  top: 0;
+  left: 0;
+  right: 0;
+
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+}
+.white {
+  z-index: 10;
+
+  /* background-color: #fff; */
+  opacity: calc(1 - var(--scroll));
+  background-image: url("src/assets/Onmyoji/Enmusubi-1.jpg");
+  /* background-image: url("src/assets/Avengers.svg"); */
+}
+.black {
+  z-index: 9;
+  /* transition: all 1s ease-in-out; */
+  /* background-color: #000; */
+  opacity: var(--scroll);
+  background-image: url("src/assets/Onmyoji/Enmusubi-2.jpg");
+}
+
+.arrow-down {
+  z-index: 100;
+  position: fixed;
+  height: 80px;
+
+  box-shadow: 20px 20px rgba(55, 55, 55, 0.4);
+
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  background-image: url("src/assets/Arrowdown.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+
+  margin: 0 auto; /* Will not center vertically and won't work in IE6/7. */
+}
+</style>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Marvel",
+  props: {},
+  setup: (props) => {},
+  data() {
+    return {};
+  },
+  methods: {
+    scrollBtnOnClick: () => {
+      console.log("aaa");
+    },
+  },
+});
+</script>
